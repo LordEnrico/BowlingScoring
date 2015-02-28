f69ead59229456d55547ed48de737b24fc831b07
Omar Qunsul <omar.qunsul@gmail.com>
2015-02-28 18:23:21 +0100

    Deleting players from the game, before it starts

--- js/app.js
+++ js/app.js
@@ -13,27 +13,47 @@ Game.prototype.renderPlayers = function() {
       var li = $('<li>', { 'class': 'list-group-item', 'id': 'player-' + i.toString() });
       li.text(player_name);
       $('#players_list ul').append(li);
-      var delete_button = $('<a>', { href: '#', 'class': 'delete-player' });
-
+      var delete_button = $('<a>', { href: '#', 'class': 'badge delete-player', 'data-player-id': i });
+      delete_button.text('X');
+      li.append(delete_button);
     }
   }
 };
 
 game = new Game();
 
+function addPlayer(playerName) {
+  playerName = playerName.trim();
+  if (playerName.length > 0) {
+    game.players.push(playerName);
+    game.renderPlayers();
+    $('#player_name').val('');
+  }
+}
+
 // Document load script
 $(function() {
-  $('#add-player-form').submit(function(ev) {
+  $('#add_player_form').submit(function(ev) {
     ev.preventDefault();
     var playerName = $('#player_name').val();
-    playerName = playerName.trim();
-    if (playerName.length > 0) {
-      game.players.push(playerName);
-      game.renderPlayers();
-      $('#player_name').val('');
-    }
+    addPlayer(playerName);
+  });
+
+  $('#add_random_players').on('click', function(ev) {
+    addPlayer('Omar');
+    addPlayer('Johannes');
+    addPlayer('Jwana');
+    addPlayer('Anna');
   });
 
   // Game render players on game beginning
   game.renderPlayers();
+
+  // Deleting player
+  $('#players_list').on('click', 'a.delete-player', function(ev) {
+    var player_id = $(ev.target).data('player-id');
+    console.log(player_id);
+    game.players.splice(player_id, 1);
+    game.renderPlayers();
+  });
 });

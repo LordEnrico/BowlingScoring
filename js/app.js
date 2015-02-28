5eb8c9e104f36369868c1299e497cf53641a9555
Omar Qunsul <omar.qunsul@gmail.com>
2015-02-28 22:28:36 +0100

    Game can start now, without any external JS dependencies

--- js/app.js
+++ js/app.js
@@ -1,59 +1,56 @@
+function element(id) {
+  return document.getElementById(id);
+}
+
 function Game() {
   this.players = [];
 }
-
-Game.prototype.renderPlayers = function() {
-  $('#players_list ul li').remove();
-  if (this.players.length == 0) {
-    $('#players_list .alert').removeClass('hidden');
-  } else {
-    $('#players_list .alert').addClass('hidden');
-    for (i = 0; i < this.players.length; i++) {
-      var player_name = this.players[i];
-      var li = $('<li>', { 'class': 'list-group-item', 'id': 'player-' + i.toString() });
-      li.text(player_name);
-      $('#players_list ul').append(li);
-      var delete_button = $('<a>', { href: '#', 'class': 'badge delete-player', 'data-player-id': i });
-      delete_button.text('X');
-      li.append(delete_button);
-    }
-  }
-};
 
 game = new Game();
 
 function addPlayer(playerName) {
   playerName = playerName.trim();
-  if (playerName.length > 0) {
-    game.players.push(playerName);
-    game.renderPlayers();
-    $('#player_name').val('');
+  if (playerName.length == 0) return;
+  game.players.push(playerName);
+  element('player_name').value = ''; // Reset the input.
+  var li = document.createElement('LI');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(playerName));
+  element('players_list').appendChild(li);
+  element('start_game').removeAttribute('disabled');
+}
+
+function startGame() {
+  element('board').style.display = '';
+  element('players').style.display = 'none';
+  for(i = 0; i < game.players.length; i++) {
+    var player = game.players[i];
+    var row = document.createElement('TR');
+    var td = document.createElement('TD'); td.appendChild(document.createTextNode(player));
+    row.appendChild(td);
+    element('scores').appendChild(row);
+
   }
 }
 
 // Document load script
-$(function() {
-  $('#add_player_form').submit(function(ev) {
+(function() {
+
+  element('add_player_form').addEventListener('submit', function(ev) {
     ev.preventDefault();
-    var playerName = $('#player_name').val();
+    var playerName = element('player_name').value;
     addPlayer(playerName);
   });
 
-  $('#add_random_players').on('click', function(ev) {
+  element('add_random_players').addEventListener('click', function(ev) {
     addPlayer('Omar');
     addPlayer('Johannes');
     addPlayer('Jwana');
     addPlayer('Anna');
   });
 
-  // Game render players on game beginning
-  game.renderPlayers();
-
-  // Deleting player
-  $('#players_list').on('click', 'a.delete-player', function(ev) {
-    var player_id = $(ev.target).data('player-id');
-    console.log(player_id);
-    game.players.splice(player_id, 1);
-    game.renderPlayers();
+  element('start_game').addEventListener('click', function(ev) {
+    startGame();
   });
-});
+
+})();
